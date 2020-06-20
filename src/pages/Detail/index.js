a7cc8644cabfde1eaab098e451e2dc66862146b6
WCFX <wcfx.dev@gmail.com>
2020-06-19 21:33:44 -0300

    Parando para realizar a criaçao do backend do app

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, TouchableOpacity, Image, Text } from 'react-native';
-// import { useNavigation } from '@react-navigation/native';
-
+import { View, TouchableOpacity, Linking, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 
 export default function Detail(){
- 
+  
+  const navigation = useNavigation();
+  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso Cadelinha atropelada no valor de R$120.00';
+
+
+  function navigateBack(){
+    navigation.goBack()
+  }
+
+  function sendMail(){
+    MailComposer.composeAsync({
+      subject: 'Heroi do caso: Cadelinha atropelada',
+      recipients: ['wagnerbfx@gmail.com'],
+      body: message,
+    })
+  }
+  function sendWhatsapp(){
+    Linking.openURL(`whatsapp://send?phone=5551997168573&text=${message}`)
+  }
+
   return(
+
     <View style={styles.container}>
       <View style={styles.header}>
 
         <Image source={logoImg} />
 
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={navigateBack}>
          <Feather name="arrow-left" size={28} color="#E02041"/>
         </TouchableOpacity>
       </View>
@@ -41,13 +61,13 @@ export default function Detail(){
           <Text style={styles.heroDescription}>Entre em contato:</Text>
 
           <View style={styles.actions}>
-          <TouchableOpacity style={styles.actions} onPress={()=> {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>
               WhatsApp
             </Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.actions} onPress={()=> {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>
               E-Mail
             </Text>
--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -22,12 +22,47 @@ export default StyleSheet.create({
   incidentProperty: {
     fontSize: 14,
     color: '#414141',
-    fontWeight: 'bold'
+    fontWeight: 'bold' 
   },
   incidentValue: {
     marginTop: 8,
     fontSize: 15,
     marginBottom: 24,
     color: '#737380'
+  },
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+  },
+  heroTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
+  heroDescription: {
+    fontSize: 15,
+    color: '#737380',
+    marginTop: 16,
+  },
+  actions: {
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  action: {
+    backgroundColor: '#E02041',
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  actionText: {
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold'
   },
 })
